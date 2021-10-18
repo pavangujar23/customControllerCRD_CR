32bfd072adabe1f7c37b1146ecec29557517c0a9
pavangujar23 <pratheek98876@gmail.com>
2021-10-18 17:32:39 +0530

    customController

---
+++ README.md
@@ -0,0 +1,11 @@
+Kluster //type name 
+
+pavangujar.dev
+v1aplha1
+
+
+generate
+1> deep copy objects
+2. clientset
+3. informers
+4. listener
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	v1aplha1 "github.com/pavangujar23/kluster/pkg/apis/pavangujar.dev/v1alpha1"
+)
+
+func main() {
+	k := v1aplha1.Kluster{}
+	fmt.Println(k)
+}
+
+// specify the global tags forward Api
+
+//controll the bheavior of code generator
+// global  and local
